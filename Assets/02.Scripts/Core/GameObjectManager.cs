77b7a2a4f53b88ab6c1af5935fcc9a2d5dbc8014
유민형 <minhyeong61022@gmail.com>
2026-07-07 16:20:57 +0900

    [유민형][System] GameObjectManager 구현
    GameObjectManager에 필요한 PoolManager, ResourceManager 구현

---
+++ Assets/02.Scripts/Core/GameObjectManager.cs
@@ -0,0 +1,42 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class GameObjectManager : MonoBehaviour
+{
+    public static GameObjectManager Instance;
+
+    private PoolManager _poolManager;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        _poolManager = new PoolManager();
+    }
+
+    public async UniTaskVoid CreateObject(string path, Vector3 spawnSpot)
+    {
+        GameObject prefab = await ResourceManager.Instance.LoadAsset<GameObject>(path);
+        if (prefab == null)
+            return;
+
+        GameObject gameObject = _poolManager.Pop(prefab);
+        if (gameObject == null)
+            return;
+
+        gameObject.transform.position = spawnSpot;
+    }
+
+    public void RequestDestroyObject(GameObject gameObject)
+    {
+        _poolManager.Push(gameObject);
+    }
+}
