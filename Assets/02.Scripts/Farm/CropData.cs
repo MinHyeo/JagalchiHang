09c8c4563dfadde59b8aa022f32e7aa0791d4d1b
김영현 <secondpale.dev@gmail.com>
2026-07-07 18:45:05 +0900

    [김영현][Farm] CropData 및 FarmPlotModel 데이터 클래스 작성

---
+++ Assets/02.Scripts/Farm/CropData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class CropData
+{
+    public string Id;
+    public string Name;
+    public string Description;
+    public int GrowthTimeSeconds;
+    public int GrowthStageCount;
+    public string SeedItemDataId;
+    public string HarvestItemDataId;
+    public int HarvestMinCount;
+    public int HarvestMaxCount;
+    public string IconPath;
+    public string PrefabPath;
+}
---
+++ Assets/02.Scripts/Farm/FarmPlotModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class FarmPlotModel
+{
+    public int PlotUniqueId;
+    public string CropDataId;
+    public bool IsPlanted;
+    public long PlantedTimeStampTicks;
+    public int CurrentGrowthStage;
+    public bool IsWatered;
+}
