1254b92702ec47750ee690863989ae0194186227
신영준 <kdrsa789@gmail.com>
2026-07-08 19:20:28 +0900

    [신영준][Monster] 몬스터 구현(체력/이동/전투)

---
+++ Assets/02.Scripts/Monster/MonsterStatProvider.cs
@@ -0,0 +1,51 @@
+﻿using Unity.Android.Gradle.Manifest;
+using UnityEngine;
+
+public class MonsterStatProvider : MonoBehaviour, IMonsterStatProvider
+{
+    [SerializeField] private string _monsterId;
+
+    private int _maxHealth;
+    private int _attackPower;
+    private float _attackRange;
+    private float _moveSpeed;
+
+    public int MaxHealth
+    {
+        get { return _maxHealth; }
+    }
+
+    public int AttackPower
+    {
+        get { return _attackPower; }
+    }
+
+    public float AttackRange
+    {
+        get { return _attackRange; }
+    }
+
+    public float MoveSpeed
+    {
+        get { return _moveSpeed; }
+    }
+
+    private void Awake()
+    {
+        MonsterData data = GameDataManager.Instance.GetData<MonsterData>();
+    
+        if (data == null)
+        {
+            _maxHealth = 100;
+            _attackPower = 10;
+            _attackRange = 2f;
+            _moveSpeed = 3.5f;
+            return;
+        }
+
+        _maxHealth = data.MaxHp;
+        _attackPower = data.BasicAttack;
+        _attackRange = data.BasicAttackRange;
+        _moveSpeed = data.BasicSpeed;
+    } 
+}
