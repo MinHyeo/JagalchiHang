2d432a1ca557c31b7e4b09c91b76dee168433621
MinHyeong Yu <minhyeong61022@gmail.com>
2026-07-09 11:17:27 +0900

    Revert "[JGC-38] 플레이어 일부 구현"

--- Assets/02.Scripts/IPlayerState.cs
+++
@@ -1,9 +0,0 @@
-﻿using JetBrains.Annotations;
-using UnityEngine;
-
-public interface IPlayerState
-{
-    void Enter(PlayerController player);
-    void Update(PlayerController player);
-    void Exit(PlayerController player);
-}
