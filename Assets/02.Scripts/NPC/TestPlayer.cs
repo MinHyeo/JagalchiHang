27ab5d4437b9385dbd604d58e0ff85cf7335d87d
orangeorae <orangeorae@naver.com>
2026-07-09 20:41:13 +0900

    [최선화][NPC] BattleNPC 플레이어 추적 기능 구현

---
+++ Assets/02.Scripts/NPC/TestPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{ 
+    // 테스트용 플레이어 코드 
+
+    public float moveSpeed = 5.0f;
+    public float jumpPower;
+
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+
+        dir.Normalize();
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse); //무게 적용하고 순간적인 힘 가하기 
+        }
+    }
+}
