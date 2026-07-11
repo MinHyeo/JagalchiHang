c0183d9de3abe75e70da855a07e78c3a3a4c20f7
유민형 <minhyeong61022@gmail.com>
2026-07-11 20:41:58 +0900

    [유민형][System] 낮과 밤 구현

--- Assets/02.Scripts/Core/TimeManager.cs
+++ Assets/02.Scripts/Core/TimeManager.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class TimeManager : SingletonBase<TimeManager>
 {
-    [Header("시간 설정")]
-    [SerializeField] private float _timeScale = 60f;
     [Header("Sun")]
     [SerializeField] private Light _sun;
     [SerializeField] private Gradient _sunColor;
@@ -14,18 +12,19 @@ public class TimeManager : SingletonBase<TimeManager>
     [SerializeField] private Gradient _moonColor;
     [SerializeField] private AnimationCurve _moonIntenstiy;
 
-    private int _year = 2026;
-    private int _month = 7;
-    private int _day = 9;
-    private int _hour = 11;
-    private int _minute = 0;
-    private float _second = 0;
+    [Header("Other Lighting")]
+    [SerializeField] private AnimationCurve _lightingIntensityMultiplier;
+    [SerializeField] private AnimationCurve _reflectionIntensityMultiplier;
 
-    public int Year => _year;
-    public int Month => _month;
-    public int Day => _day;
-    public int Hour => _hour;
-    public int Minute => _minute;
+    public int Day;
+    public int Hour;
+    public int Minute;
+
+    private float _time;
+    private float _fullDayTime = 30f;
+    private float _startTime = 0.4f;
+    private float _timeRate;
+    private Vector3 _noon = new Vector3(90, 0, 0);
 
     public event Action OnMinuteChanged;
     public event Action OnHourChanged;
@@ -33,78 +32,41 @@ public class TimeManager : SingletonBase<TimeManager>
 
     private void OnEnable()
     {
-        OnMinuteChanged += UpdateDayNightCycle;
-    }
-
-    private void OnDisable()
-    {
-        OnMinuteChanged = null;
-        OnHourChanged = null;
-        OnDayChanged = null;
+        _timeRate = 1.0f / _fullDayTime;
+        _time = _startTime;
     }
 
     private void Update()
     {
-        UpdateTime();
-    }
+        _time = (_time + _timeRate * Time.deltaTime) % 1.0f;
 
-    private void UpdateTime()
-    {
-        _second = _second + _timeScale * Time.deltaTime;
-
-        if(_second >= 60)
-        {
-            int plusMinute = (int)_second / 60;
-            _second %= 60;
-            _minute = _minute + plusMinute;
-            OnMinuteChanged?.Invoke();
-        }
-
-        if(_minute >= 60)
-        {
-            int plusHour = _minute / 60;
-            _minute %= 60;
-            _hour = _hour + plusHour;
-            OnHourChanged?.Invoke();
-        }
-
-        if(_hour >= 24)
-        {
-            int plusDay = _hour / 24;
-            _hour %= 24;
-            _day = _day + plusDay;
-            OnDayChanged?.Invoke();
-        }
-    }
-
-    private void UpdateDayNightCycle()
-    {
         UpdateLighting(_sun, _sunColor, _sunIntenstiy);
         UpdateLighting(_moon, _moonColor, _moonIntenstiy);
+
+        RenderSettings.ambientIntensity = _lightingIntensityMultiplier.Evaluate(_time);
+        RenderSettings.reflectionIntensity = _reflectionIntensityMultiplier.Evaluate(_time);
     }
 
     private void UpdateLighting(Light light, Gradient gradient, AnimationCurve animationCurve)
     {
-        float time = ((_hour + 6) % 12 + Minute / 60.0f) / 12.0f;
-        float intensity = _sunIntenstiy.Evaluate(time);
-        Color color = _sunColor.Evaluate(time);
+        if (light == null)
+            return;
 
+        float intensity = animationCurve.Evaluate(_time);
+        Color color = gradient.Evaluate(_time);
 
-        float radius = ((360 / 24) * _hour - 90);
-        radius += (15.0f / 60) * _minute;
-
-        light.transform.rotation = Quaternion.Euler(radius, -30, 0);
+        light.transform.eulerAngles = (_time - (light == _sun ? 0.25f : 0.75f)) * _noon * 4.0f;
         light.color = color;
         light.intensity = intensity;
 
-        GameObject gameObject = light.gameObject;
-        if(light.intensity == 0 && gameObject.activeInHierarchy == true)
+        GameObject lightObject = light.gameObject;
+        if(light.intensity == 0 && lightObject.activeInHierarchy == true)
         {
-            gameObject.SetActive(false);
+            lightObject.SetActive(false);
         }
-        else if(light.intensity > 0 && gameObject.activeInHierarchy == false)
+        else if(light.intensity > 0 && lightObject.activeInHierarchy == false)
         {
-            gameObject.SetActive(true);
+            lightObject.SetActive(true);
         }
     }
 }
