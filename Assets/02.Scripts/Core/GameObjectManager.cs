bcf708b57f1efedaf371730e9f97d0820d740966
유민형 <minhyeong61022@gmail.com>
2026-07-12 17:56:33 +0900

    [유민형][System] 오브젝트 생성 시 초기화 기능 구현

--- Assets/02.Scripts/Core/GameObjectManager.cs
+++ Assets/02.Scripts/Core/GameObjectManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class GameObjectManager : SingletonBase<GameObjectManager>
 {
+    private static int _objectInstanceKeyGenerator = 0;
+
     private Transform _rootTransform;
     private PoolManager _poolManager;
 
@@ -12,12 +14,12 @@ public class GameObjectManager : SingletonBase<GameObjectManager>
         _rootTransform = this.transform;
     }
 
-    public void CreateObject(string path, Vector3 spawnSpot)
+    public void CreateObject(string dataId, string path, Vector3 spawnSpot)
     {
-        CreateObjectAsync(path, spawnSpot).Forget();
+        CreateObjectAsync(dataId, path, spawnSpot).Forget();
     }
 
-    public async UniTaskVoid CreateObjectAsync(string path, Vector3 spawnSpot)
+    public async UniTaskVoid CreateObjectAsync(string dataId, string path, Vector3 spawnSpot)
     {
         GameObject prefab = await ResourceManager.Instance.LoadAsset<GameObject>(path);
         if (prefab == null)
@@ -29,6 +31,18 @@ public class GameObjectManager : SingletonBase<GameObjectManager>
 
         gameObject.transform.SetParent(_rootTransform);
         gameObject.transform.position = spawnSpot;
+        AddObjectOnCreate(gameObject, dataId)
+    }
+
+    private void AddObjectOnCreate(GameObject createdObject, string dataId)
+    {
+        int generatedInstanceId = _objectInstanceKeyGenerator++;
+        ISpawnable createdScript = createdObject.GetComponent<ISpawnable>();
+
+        if (createdScript == null)
+            return;
+
+        createdScript.Init(generatedInstanceId, dataId);
     }
 
     public void RequestDestroyObject(GameObject gameObject)
