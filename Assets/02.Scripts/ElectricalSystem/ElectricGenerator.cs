6535b93fbbe427dd7b12f050bb04135f2f553b9b
유민형 <minhyeong61022@gmail.com>
2026-07-13 17:34:23 +0900

    [유민형][Electrical] 전기 고장 시 전기 파티클과 톱니바퀴가 회전하지 않는 기능 구현

--- Assets/02.Scripts/ElectricalSystem/ElectricGenerator.cs
+++ Assets/02.Scripts/ElectricalSystem/ElectricGenerator.cs
@@ -2,7 +2,10 @@
 
 public class ElectricGenerator : MonoBehaviour
 {
-    private bool _isBroken = false;
+    private bool _isBroken;
+
+    private ParticleSystem _electricParticle;
+    private RotateSubmarineBlades _rotateBlades;
 
     private int _maxPower = 100;
     private int _currentPower;
@@ -10,15 +13,23 @@ public class ElectricGenerator : MonoBehaviour
     private int _troublePower = 500;
     private int _currentTroblePower;
 
+    private void Awake()
+    {
+        _electricParticle = GetComponent<ParticleSystem>();
+        _rotateBlades = GetComponent<RotateSubmarineBlades>();
+    }
+
     private void OnEnable()
     {
         _currentPower = _maxPower;
         _currentPower = 0;
+
+        RotateBlade();
     }
 
     public int UseGenerator(int amount)
     {
-        if (_isBroken)
+        if (_rotateBlades.rotate == false)
             return 0;
 
         int power = amount;
@@ -36,6 +47,8 @@ public class ElectricGenerator : MonoBehaviour
         _currentPower = _currentPower - power;
         _currentTroblePower = _currentTroblePower + power;
 
+        RotateBlade();
+
         Debug.Log($"발전기 사용 : {_currentPower}");
         return power;
     }
@@ -43,19 +56,28 @@ public class ElectricGenerator : MonoBehaviour
     private void BreakGenerator()
     {
         Debug.LogWarning("발전기 고장!!");
+        _electricParticle.Play();
         _isBroken = true;
     }
 
     public void FixGenerator()
     {
         Debug.Log("발전기 수리");
-        _isBroken = false;
         _currentTroblePower = 0;
+        _electricParticle.Pause();
+        _isBroken = false;
+        RotateBlade();
     }
 
     public void ReCharageGenerator(int amount)
     {
         Debug.Log($"발전기 충전 : {_currentPower} => {Mathf.Min(_currentPower + amount, _maxPower)}");
         _currentPower = Mathf.Min(_currentPower + amount, _maxPower);
+        RotateBlade();
+    }
+
+    private void RotateBlade()
+    {
+        _rotateBlades.rotate = _currentPower > 0 && _isBroken == false;
     }
 }
