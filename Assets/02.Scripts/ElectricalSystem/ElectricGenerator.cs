0a0c3a23b7f45318a35a15ed64d7f105150244f5
유민형 <minhyeong61022@gmail.com>
2026-07-13 15:56:05 +0900

    [유민형][Electrical] 전기 사용/충전/고장 구현

---
+++ Assets/02.Scripts/ElectricalSystem/ElectricGenerator.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class ElectricGenerator : MonoBehaviour
+{
+    private bool _isBroken = false;
+
+    private int _maxPower = 100;
+    private int _currentPower;
+
+    private int _troublePower = 500;
+    private int _currentTroblePower;
+
+    private void OnEnable()
+    {
+        _currentPower = _maxPower;
+        _currentPower = 0;
+    }
+
+    public int UseGenerator(int amount)
+    {
+        if (_isBroken)
+            return 0;
+
+        int power = amount;
+
+        if(_currentPower < power)
+        {
+            power = _currentPower;
+        }
+
+        if(_currentTroblePower + power >= _troublePower)
+        {
+            power = power - ((_currentTroblePower + power) - _troublePower);
+            BreakGenerator();
+        }
+        _currentPower = _currentPower - power;
+        _currentTroblePower = _currentTroblePower + power;
+
+        Debug.Log($"발전기 사용 : {_currentPower}");
+        return power;
+    }
+
+    private void BreakGenerator()
+    {
+        Debug.LogWarning("발전기 고장!!");
+        _isBroken = true;
+    }
+
+    public void FixGenerator()
+    {
+        Debug.Log("발전기 수리");
+        _isBroken = false;
+        _currentTroblePower = 0;
+    }
+
+    public void ReCharageGenerator(int amount)
+    {
+        Debug.Log($"발전기 충전 : {_currentPower} => {Mathf.Min(_currentPower + amount, _maxPower)}");
+        _currentPower = Mathf.Min(_currentPower + amount, _maxPower);
+    }
+}
