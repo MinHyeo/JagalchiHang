6a9a3dadbba76950be0cfd33b35e1650caa4a38c
유민형 <minhyeong61022@gmail.com>
2026-07-14 19:44:34 +0900

    [유민형][System] NetworkManager 구현

---
+++ Assets/02.Scripts/Core/NetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class NetworkManager : SingletonBase<NetworkManager>
+{
+    private string _saveFilePath;
+
+    private void Start()
+    {
+        _saveFilePath = Path.Combine(Application.persistentDataPath, "saveData.json");
+    }
+
+    public void SaveGame(SaveModel saveModel)
+    {
+        string jsonText = JsonUtility.ToJson(saveModel, true);
+
+        File.WriteAllText(_saveFilePath, jsonText);
+
+        Debug.Log($"저장 완료: {_saveFilePath}");
+    }
+
+    public SaveModel LoadGame()
+    {
+        if (File.Exists(_saveFilePath) == false)
+            return new SaveModel();
+
+        string jsonText = File.ReadAllText(_saveFilePath);
+        SaveModel saveModel = JsonUtility.FromJson<SaveModel>(jsonText);
+        return saveModel;
+    }
+}
