7bcf690da4f1e38ab4e9c1c042bae670c91e9cde
이나라 <dlskfk72@gmail.com>
2026-07-14 18:11:17 +0900

    [이나라][Player] PlayerModel에 FarmPlotList 추가

--- Assets/02.Scripts/Player/MVVM/PlayerModel.cs
+++ Assets/02.Scripts/Player/MVVM/PlayerModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 [Serializable]
 public class PlayerModel
@@ -8,4 +9,6 @@ public class PlayerModel
     public int CurrentHp;
     public int CurrentHunger;
     public int CurrentThirst;
+
+    public List<FarmPlotModel> FarmPlotList = new List<FarmPlotModel>();
 }
