023887531d4375ef4ee8b27b2feffb864da2587e
유민형 <minhyeong61022@gmail.com>
2026-07-21 17:43:25 +0900

    [유민형][Map] 플레이어 벙커 및 파밍 맵 이동 구현

--- Assets/02.Scripts/Core/WorldManager.cs
+++ Assets/02.Scripts/Core/WorldManager.cs
@@ -23,7 +23,23 @@ public class WorldManager
         //_npcManager.Init(target);
     }
 
-    private void CreateManager()
+    public void TransMap(MapType mapType)
+    {
+        // 기존 맵 제거 및 새로운 맵 불러오기
+        _mapManager.ChangeMap(mapType);
+
+        // 플레이어 위치 받아오기
+        Vector3 spawnPosition = _mapManager.GetMapSpawnPosition();
+
+        // 플레이어 위치 적용
+        _playerManager.TransPlayerPosition(spawnPosition);
+
+        // Farm 상태 갱신
+
+        // Npc 상태 갱신
+    }
+
+private void CreateManager()
     {
         _playerManager = new PlayerManager();
         _monsterManager = new MonsterManager();
---
+++ Assets/02.Scripts/Map/MapPortal.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MapPortal : MonoBehaviour, IInteractionable
+{
+    [SerializeField] private MapType _transMapType;
+
+    public void Interaction()
+    {
+        Debug.Log("맵 이동");
+        GameManager.Instance.RequestMapChange(_transMapType);
+    }
+}
