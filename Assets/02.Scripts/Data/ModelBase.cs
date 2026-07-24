34421ed4ed9169acb758871967bd5ccf2bd5acfe
이선학 <mumahub1219@gmail.com>
2026-07-24 17:28:07 +0900

    [이선학][UI] 아이템 모델 이전

--- Assets/02.Scripts/Data/ModelBase.cs
+++ Assets/02.Scripts/Data/ModelBase.cs
@@ -3,6 +3,13 @@
     public int modelId;
 }
 
+public enum ItemLocationType
+{
+    None,
+    Inventory = 1,
+    Storage = 2,
+}
+
 [System.Serializable]
 public class SaveModel : ModelBase
 {
@@ -16,6 +23,10 @@ public class PlayerSaveModel
 }
 
 public class ItemSaveModel 
-{ 
-
+{
+    public long ItemUniqueId;
+    public string ItemDataId;
+    public int ItemStackCount;
+    public ItemLocationType Location;
+    public int SlotIndex;
 }
