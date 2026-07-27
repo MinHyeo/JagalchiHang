ad1de3280f7f51031d39e82cddc4c6a0b142872c
최선화 <orangeorae@naver.com>
2026-07-27 16:17:06 +0900

    [최선화][NPC] NPC 벙커 오류 수정

--- Assets/02.Scripts/Player/Manager/PlayerManager.cs
+++ Assets/02.Scripts/Player/Manager/PlayerManager.cs
@@ -39,7 +39,7 @@ public class PlayerManager : ITargetable
         
         if (_playerSpawnPos == null) return;
 
-        _player = await GameObjectManager.Instance.CreateObjectAsync("Player_1", "Prefab/Player", _playerSpawnPos + Vector3.up * 5f);
+        _player = await GameObjectManager.Instance.CreateObjectAsync("Player_1", "Prefab/Player", _playerSpawnPos);
         if (_player == null) return;
 
         Debug.Log($"플레이어가 생성됐다!");
